bffc87c73a051bf9d4dbde7937382bd82ba2a7cb
danivisser <d.visser@tilburguniversity.edu>
2023-09-20 20:40:54 +0200

    The new files to download, merge and remove unnecessary variables

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,5 +1,6 @@
 #set working directory to the location of this script
 
+#download files
 download.file('https://datasets.imdbws.com/title.basics.tsv.gz','../../data/title.basics.tsv.gz')
 download.file('https://datasets.imdbws.com/title.ratings.tsv.gz','../../data/title.ratings.tsv.gz')
 
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,9 +1,12 @@
-# Load datasets into R 
-df1 <- read.csv("./gen/data-preparation/input/dataset1.csv")
-df2 <- read.csv("./gen/data-preparation/input/dataset2.csv")
+library(data.table)
+library(tidyverse)
 
-# Merge on id
-df_merged <- merge(df1,df2,by="id")
+# Load datasets into R 
+titles <- setDT(read_tsv(gzfile('../../data/title.basics.tsv.gz')))
+ratings <- setDT(read_tsv(gzfile('../../data/title.ratings.tsv.gz')))
+
+# Merge on tconst
+IMDb_merged<-left_join(titles,ratings,by="tconst")
 
 # Save merged data
-save(df_merged,file="./gen/data-preparation/temp/data_merged.RData")
+save(IMDb_merged, file = "../../gen/data-preparation/temp/data_merged.Rdata")
