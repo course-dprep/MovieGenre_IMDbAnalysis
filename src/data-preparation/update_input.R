0ed4d185017ab0dcd54a5f5df10a255e8cd14f4d
DirkBeekmans <d.a.beekmans@tilburguniversity.edu>
2023-10-03 15:52:22 +0200

    removing src files that are not necessary anymore, and one empty folder in gen/data-preparation

--- src/data-preparation/update_input.R
+++
@@ -1,5 +0,0 @@
-# Copy the raw data into input folder 
-# This step really depends no how files are shared across the different stages (e.g. if whole pipeline
-# is on a single machine, could directly access data from data directory)
-file.copy("./data/title.basics.tsv.gz","./gen/data-preparation/input/title.basics.tsv.gz")
-file.copy("./data/title.ratings.tsv.gz","./gen/data-preparation/input/title.ratings.tsv.gz")
