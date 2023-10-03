edd4adf6a78e739faf12434ca4062979d8f61e59
DirkBeekmans <d.a.beekmans@tilburguniversity.edu>
2023-10-03 15:41:34 +0200

    modified the merge.R file, put a note at the makefile on root directory and removed empty folder

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
 # Read filtered csv
-titles<- read_csv("../../data/titles_movies_only.csv")
+titles_movies_only<- read_csv("../../data/titles_movies_only.csv")
 
 # Read rating data set
 ratings <- read_tsv(gzfile('../../data/title.ratings.tsv.gz'))
 
 # Merge on tconst
-IMDb_merged<-left_join(titles,ratings,by="tconst")
+IMDb_merged<-left_join(titles_movies_only,ratings,by="tconst")
 
 # Save merged data
 write_csv(IMDb_merged, file = "../../data/IMDb_merged.csv")
